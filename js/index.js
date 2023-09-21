523d7218eb5108e3e8c116c31251578372b529b5
arsk-11 <ambalicekeling53@gmail.com>
2023-09-21 10:24:36 +0700

    upload new release

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let btnEpx = document.getElementById("EpxBtn");
+let DivA = document.querySelector(".ad-card");
+btnEpx.addEventListener("click", () => {
+    DivA.style
+})
+// function btnEpx() {
+//     alert(1)
+// }
